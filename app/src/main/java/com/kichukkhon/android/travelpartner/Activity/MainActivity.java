6fb3bd1c0ebf798b730d74c9f68252302b7938f2
Ratul <ratul.bside@outlook.com>
2016-08-25 02:34:44 +0600

    Way to navigate tour home from home activity (temporary)

--- app/src/main/java/com/kichukkhon/android/travelpartner/Activity/MainActivity.java
+++ app/src/main/java/com/kichukkhon/android/travelpartner/Activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kichukkhon.android.travelpartner.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.kichukkhon.android.travelpartner.R;
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnTourHomeClick(View view) {
+        Intent intent = new Intent(this, TourHomeActivity.class);
+        startActivity(intent);
     }
 }
