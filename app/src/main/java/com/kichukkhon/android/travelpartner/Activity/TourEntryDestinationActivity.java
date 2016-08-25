4ecaccc5fb6fa8b26990821585aa9a862a4067fa
Ratul <ratul.bside@outlook.com>
2016-08-25 19:14:52 +0600

    Choose destination with place autocomplete

--- app/src/main/java/com/kichukkhon/android/travelpartner/Activity/TourEntryDestinationActivity.java
+++ app/src/main/java/com/kichukkhon/android/travelpartner/Activity/TourEntryDestinationActivity.java
@@ -1,15 +1,173 @@
 package com.kichukkhon.android.travelpartner.Activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.kichukkhon.android.travelpartner.Adapter.PlaceAutocompleteAdapter;
 import com.kichukkhon.android.travelpartner.R;
+import com.kichukkhon.android.travelpartner.Util.Constants;
 
-public class TourEntryDestinationActivity extends AppCompatActivity {
+public class TourEntryDestinationActivity extends BaseActivity
+        implements GoogleApiClient.OnConnectionFailedListener {
+
+    public static String TAG = "DestinationActivity";
+
+    /**
+     * GoogleApiClient wraps our service connection to Google Play Services and provides access
+     * to the user's sign in state as well as the Google's APIs.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    private PlaceAutocompleteAdapter mAdapter;
+
+    AutoCompleteTextView mAutocompleteDestination;
+
+    private static final LatLngBounds BOUNDS_BANGLADESH = new LatLngBounds(
+            new LatLng(20.7464106, 88.0085887), new LatLng(26.6342433, 92.6801153));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Construct a GoogleApiClient for the {@link Places#GEO_DATA_API} using AutoManage
+        // functionality, which automatically sets up the API client to handle Activity lifecycle
+        // events. If your activity does not extend FragmentActivity, make sure to call connect()
+        // and disconnect() explicitly.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, 0 /* clientId */, this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+
         setContentView(R.layout.activity_tour_entry_destination);
+
+        mAutocompleteDestination = (AutoCompleteTextView) findViewById(R.id.autocomplete_destination);
+
+        // Register a listener that receives callbacks when a suggestion has been selected
+        mAutocompleteDestination.setOnItemClickListener(mAutocompleteClickListener);
+
+        //Buid a AutocompleteFilter to restrict suggestions only to cities
+        AutocompleteFilter.Builder builder = new AutocompleteFilter.Builder();
+        builder.setTypeFilter(AutocompleteFilter.TYPE_FILTER_CITIES);
+
+        // Set up the adapter that will retrieve suggestions from the Places Geo Data API that cover
+        // the entire world.
+        mAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, BOUNDS_BANGLADESH,
+                builder.build());
+        mAutocompleteDestination.setAdapter(mAdapter);
+    }
+
+    /**
+     * Listener that handles selections from suggestions from the AutoCompleteTextView that
+     * displays Place suggestions.
+     * Gets the place id of the selected item and issues a request to the Places Geo Data API
+     * to retrieve more details about the place.
+     *
+     * @see com.google.android.gms.location.places.GeoDataApi#getPlaceById(com.google.android.gms.common.api.GoogleApiClient,
+     * String...)
+     */
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            /*
+             Retrieve the place ID of the selected item from the Adapter.
+             The adapter stores each Place suggestion in a AutocompletePrediction from which we
+             read the place ID and title.
+              */
+            final AutocompletePrediction item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+            final CharSequence primaryText = item.getPrimaryText(null);
+
+            Log.i(TAG, "Autocomplete item selected: " + primaryText);
+
+            /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+             details about the place.
+              */
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+
+            Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
+                    Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "Called getPlaceById to get Place details for " + placeId);
+        }
+    };
+
+    /**
+     * Callback for results from a Places Geo Data API query that shows the first place result in
+     * the details view on screen.
+     */
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                // Request did not complete successfully
+                Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            // Get the Place object from the buffer.
+            final Place place = places.get(0);
+
+            //get place info
+            String placeName = place.getName().toString();
+            String placeId = place.getId();
+            LatLng latLng = place.getLatLng();
+            double latitude = latLng.latitude;
+            double longitude = latLng.longitude;
+
+            Log.i(TAG, "Place details received: " + place.getName());
+
+            places.release();
+
+            //redirect to details entry
+            Intent intent = new Intent(getBaseContext(), TourEntryDetailsActivity.class);
+
+            Bundle extras = new Bundle();
+            extras.putString(Constants.PLACE_ID_KEY, placeId);
+            extras.putString(Constants.PLACE_NAME_KEY, placeName);
+            extras.putDouble(Constants.PLACE_LATITUDE_KEY, latitude);
+            extras.putDouble(Constants.PLACE_LONGITUDE_KEY, longitude);
+
+            intent.putExtras(extras);
+            startActivity(intent);
+        }
+    };
+
+    /**
+     * Called when the Activity could not connect to Google Play services and the auto manager
+     * could resolve the error automatically.
+     * In this case the API is not available and notify the user.
+     *
+     * @param connectionResult can be inspected to determine the cause of the failure
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Log.e(TAG, "onConnectionFailed: ConnectionResult.getErrorCode() = "
+                + connectionResult.getErrorCode());
+
+        // TODO(Developer): Check error code and notify the user of error state and resolution.
+        Toast.makeText(this,
+                "Could not connect to Google API Client: Error " + connectionResult.getErrorCode(),
+                Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/kichukkhon/android/travelpartner/Util/Constants.java
@@ -0,0 +1,13 @@
+package com.kichukkhon.android.travelpartner.Util;
+
+/**
+ * Created by Ratul on 8/25/2016.
+ */
+public final class Constants {
+
+    //place helper strings
+    public static final String PLACE_ID_KEY = "place_id";
+    public static final String PLACE_NAME_KEY = "place_name";
+    public static final String PLACE_LATITUDE_KEY = "place_lat";
+    public static final String PLACE_LONGITUDE_KEY = "place_lng";
+}
