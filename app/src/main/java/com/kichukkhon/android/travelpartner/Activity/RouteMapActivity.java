b75562d045e27ec5bdacc2b92f435318b419aa0e
Ratul <ratul.bside@gmail.com>
2016-08-28 02:23:13 +0600

    Create files for route tracker

---
+++ app/src/main/java/com/kichukkhon/android/travelpartner/Activity/RouteMapActivity.java
@@ -0,0 +1,15 @@
+package com.kichukkhon.android.travelpartner.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kichukkhon.android.travelpartner.R;
+
+public class RouteMapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_map);
+    }
+}
