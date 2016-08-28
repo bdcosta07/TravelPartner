a4e77177cfb137ae584ac6d5ba3c01fe707c5e0f
Ratul <ratul.bside@outlook.com>
2016-08-28 19:38:40 +0600

    Tour list activity as home and empty view for recycler view

--- app/src/main/java/com/kichukkhon/android/travelpartner/Fragment/UpcomingTourListFragment.java
+++ app/src/main/java/com/kichukkhon/android/travelpartner/Fragment/UpcomingTourListFragment.java
@@ -1,13 +1,13 @@
 package com.kichukkhon.android.travelpartner.Fragment;
 
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.kichukkhon.android.travelpartner.Adapter.TourAdapter;
 import com.kichukkhon.android.travelpartner.Class.Tour;
@@ -19,15 +19,28 @@ import java.util.ArrayList;
 public class UpcomingTourListFragment extends Fragment {
     RecyclerView recyclerView;
     TourAdapter tourAdapter;
+    TextView emptyView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        recyclerView=(RecyclerView)inflater.inflate(
-                R.layout.recycler_view,container,false);
-        tourAdapter=new TourAdapter(recyclerView.getContext(),getTourData());
-        recyclerView.setAdapter(tourAdapter);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        emptyView=(TextView) inflater.inflate(
+                R.layout.empty_view_for_recyclerview, container, false);
+
+        if (getTourData().isEmpty()) {
+            recyclerView.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        }
+        else {
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+
+            tourAdapter = new TourAdapter(recyclerView.getContext(), getTourData());
+            recyclerView.setAdapter(tourAdapter);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        }
 
         return recyclerView;
     }
@@ -42,9 +55,9 @@ public class UpcomingTourListFragment extends Fragment {
         tourDBManager=new TourDBManager(this);
     }*/
 
-    public ArrayList<Tour> getTourData(){
-        TourDBManager tourDBManager=new TourDBManager(getContext());
-        ArrayList<Tour> tourList=tourDBManager.getAllTourInfo();
+    public ArrayList<Tour> getTourData() {
+        TourDBManager tourDBManager = new TourDBManager(getContext());
+        ArrayList<Tour> tourList = tourDBManager.getAllTourInfo();
 
         return tourList;
 
