5b5bd021e9d7f60fe7183ac48d59d19935639b1e
Ratul <ratul.bside@outlook.com>
2016-08-28 22:23:34 +0600

    onclick for tour list

--- app/src/main/java/com/kichukkhon/android/travelpartner/Adapter/TourAdapter.java
+++ app/src/main/java/com/kichukkhon/android/travelpartner/Adapter/TourAdapter.java
@@ -1,24 +1,27 @@
 package com.kichukkhon.android.travelpartner.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.kichukkhon.android.travelpartner.Activity.TourDetailsActivity;
 import com.kichukkhon.android.travelpartner.Class.Tour;
 import com.kichukkhon.android.travelpartner.R;
 import com.kichukkhon.android.travelpartner.Util.AppUtils;
+import com.kichukkhon.android.travelpartner.Util.Constants;
 
 import java.util.ArrayList;
 
 /**
  * Created by Bridget on 8/24/2016.
  */
-public class TourAdapter extends RecyclerView.Adapter<TourAdapter.ViewHolder>{
+public class TourAdapter extends RecyclerView.Adapter<TourAdapter.ViewHolder> {
 
-    ArrayList<Tour> tourList=new ArrayList<>();
+    ArrayList<Tour> tourList = new ArrayList<>();
     Context context;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -32,38 +35,41 @@ public class TourAdapter extends RecyclerView.Adapter<TourAdapter.ViewHolder>{
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, TourDetailsActivity.class);
+                    intent.putExtra(Constants.CURRENT_TOUR_ID_KEY, tourList.get(getAdapterPosition()).getTourName());
+                    context.startActivity(intent);
                 }
             });
 
-            tvTourName=(TextView) itemView.findViewById(R.id.tvTourName);
-            tvStarDate=(TextView) itemView.findViewById(R.id.tvStartDate);
-            tvEndDate=(TextView)itemView.findViewById(R.id.tvEndDate);
+            tvTourName = (TextView) itemView.findViewById(R.id.tvTourName);
+            tvStarDate = (TextView) itemView.findViewById(R.id.tvStartDate);
+            tvEndDate = (TextView) itemView.findViewById(R.id.tvEndDate);
         }
     }
 
-    public TourAdapter(Context context,ArrayList<Tour> tourList) {
-        this.tourList=tourList;
+    public TourAdapter(Context context, ArrayList<Tour> tourList) {
+        this.tourList = tourList;
         this.context = context;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        View rowView= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.tour_row_layout,viewGroup,false);
-        ViewHolder viewHolder=new ViewHolder(rowView);
+        View rowView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.tour_row_layout, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(rowView);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
-        String tourName=tourList.get(position).getTourName();
+        String tourName = tourList.get(position).getTourName();
         viewHolder.tvTourName.setText(tourName);
 
-        long startDate=tourList.get(position).getStartDateTime();
+        long startDate = tourList.get(position).getStartDateTime();
         viewHolder.tvStarDate.setText(AppUtils.getFriendlyDayString(context, startDate));
 
-        long endDate=tourList.get(position).getEndDateTime();
-        viewHolder.tvEndDate.setText(AppUtils.getFriendlyDayString(context,endDate));
+        long endDate = tourList.get(position).getEndDateTime();
+        viewHolder.tvEndDate.setText(AppUtils.getFriendlyDayString(context, endDate));
     }
 
     @Override
