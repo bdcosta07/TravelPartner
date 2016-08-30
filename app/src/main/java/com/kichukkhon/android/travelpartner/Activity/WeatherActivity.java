b1793c6ad112a1cfe555e03c943f72e6f364765e
Ratul <ratul.bside@outlook.com>
2016-08-30 13:39:05 +0600

    remove unused codes from weather activity

--- app/src/main/java/com/kichukkhon/android/travelpartner/Activity/WeatherActivity.java
+++ app/src/main/java/com/kichukkhon/android/travelpartner/Activity/WeatherActivity.java
@@ -37,7 +37,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class WeatherActivity extends BaseDrawerActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class WeatherActivity extends BaseDrawerActivity {
 
     private static final int NUM_PAGES = 2;
     private ViewPager mPager;
@@ -45,22 +45,6 @@ public class WeatherActivity extends BaseDrawerActivity implements GoogleApiClie
 
     private ImageView[] dots;
     LinearLayout linearLayout;
-
-    /**
-     * Provides the entry point to Google Play services.
-     */
-    protected GoogleApiClient mGoogleApiClient;
-    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 1;
-    ProgressDialog dialog;
-
-    /**
-     * Represents a geographical location.
-     */
-    protected Location mLastLocation;
-
-    private MenuItem mSearchAction;
-    private boolean isSearchOpened = false;
-    private EditText txtSearchLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,9 +68,6 @@ public class WeatherActivity extends BaseDrawerActivity implements GoogleApiClie
                 drawPageSelectionIndicators(position);
             }
         });
-
-        dialog = new ProgressDialog(WeatherActivity.this);
-
     }
 
     private ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
@@ -165,53 +146,6 @@ public class WeatherActivity extends BaseDrawerActivity implements GoogleApiClie
         }
     }
 
-    private void searchCities() {
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle(this.getString(R.string.search_title));
-        final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        input.setMaxLines(1);
-        input.setSingleLine(true);
-        alert.setView(input, 32, 0, 32, 0);
-        alert.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                String result = input.getText().toString();
-                if (!result.isEmpty()) {
-                    SettingsUtils.saveLocation(result, WeatherActivity.this);
-                    notifyViewPagerDataSetChanged();
-                }
-            }
-        });
-        alert.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                // Cancelled
-            }
-        });
-        alert.show();
-    }
-
-    void GetCityByLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    PERMISSION_ACCESS_COARSE_LOCATION);
-        }
-        dialog.setMessage("getting your location");
-        dialog.setCanceledOnTouchOutside(false);
-        dialog.show();
-
-        buildGoogleApiClient();
-        mGoogleApiClient.connect();
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
     private void notifyViewPagerDataSetChanged() {
         //Log.d(TAG, "\nnotifyDataSetChanged()");
         mPagerAdapter.notifyDataSetChanged();
@@ -220,55 +154,10 @@ public class WeatherActivity extends BaseDrawerActivity implements GoogleApiClie
     @Override
     protected void onStart() {
         super.onStart();
-        if (mGoogleApiClient != null)
-            mGoogleApiClient.connect();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected())
-            mGoogleApiClient.disconnect();
     }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // Provides a simple way of getting a device's location and is well suited for
-        // applications that do not require a fine-grained location and that do not need location
-        // updates. Gets the best and most recent location currently available, which may be null
-        // in rare cases when a location is not available.
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if (mLastLocation != null) {
-                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-                List<Address> addresses = null;
-                try {
-                    addresses = geocoder.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                String cityName = addresses.get(0).getLocality();
-                SettingsUtils.saveLocation(cityName, this);
-
-                notifyViewPagerDataSetChanged();
-            } else {
-                Toast.makeText(this, "No location is detected", Toast.LENGTH_LONG).show();
-            }
-
-
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        mGoogleApiClient.connect();
-    }
-
 }
